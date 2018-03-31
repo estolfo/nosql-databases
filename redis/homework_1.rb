d63f839291526999be4241b6c274c100109c47c2
Emily <emily@mongodb.com>
2018-03-31 17:35:45 -0400

    Add json files and script for homework 2

--- redis/homework_1.rb
+++
@@ -1,8 +0,0 @@
-# Require the httparty gem
-
-
-# Set up the url to send a GET request to. The base url is:
-# "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"
-
-
-# Make the request and print out the "url" key in the response, which is the image url
