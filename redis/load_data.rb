1075a2d7f84598acf0f475b693f6089fb789ee91
Emily <emily@mongodb.com>
2018-04-04 12:06:29 -0400

    Update load data script to use today's timestamp for articles

--- redis/load_data.rb
+++ redis/load_data.rb
@@ -15,11 +15,13 @@ def setup_data(redis)
       redis.hset("article:#{id}", k, v)
     end
   end
+  puts "#{articles.size} articles loaded into the database as hashes."
 
   # set up time zset (the times that the articles were posted), key is 'time:'
   articles.each_with_index do |article, i|
-    redis.zadd("time:", article[:time], "article:#{i}")
+    redis.zadd("time:", Time.now.to_f, "article:#{i}")
   end
+  puts "#{articles.size} time stamps loaded into the database as a sorted set."
 
   # set up score zset (the scores that the articles have), key is score:
   scores_file = File.new(SCORES_FILE)
@@ -27,6 +29,7 @@ def setup_data(redis)
   scores.each do |article, score|
     redis.zadd("score:", score, article)
   end
+  puts "#{scores.size} scores loaded into the database as a sorted set."
 
   # set up set, key is "voted:<article number>", values are user:<user id>
   votes_files = File.new(VOTES_FILE)
@@ -38,6 +41,7 @@ def setup_data(redis)
       redis.sadd("voted:#{article_id}", user)
     end
   end
+  puts "#{votes.size} voting histories loaded into the database as a set."
 end
 
 setup_data(Redis.new)
