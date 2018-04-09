43fc3defc91d3a38cfb2b8f68752fe5883dd0a6e
GuanmingQiao <gq2135@columbia.edu>
2018-04-09 14:49:23 -0400

    hw1 leftover:

--- redis/homework_1.py
+++ redis/homework_1.py
@@ -1,6 +1,6 @@
 import requests
 url = 'https://api.nasa.gov/planetary/apod'
-param = {'api_key' : 'mLATqV8OFmD0DSqwu1ckFscnNatKVYxDpndwZcOa','date' : '1996-09-12'}
+param = {'api_key' : 'mLATqV8OFmD0DSqwu1ckFscnNatKVYxDpndwZcOa','date' : '2017-09-12'}
 r = requests.get(url, params = param)
 j = r.json()
 print(j["url"])
