84539f6db601e6552546014bb9c666104eefc91f
GuanmingQiao <gq2135@columbia.edu>
2018-04-17 00:42:43 -0400

    DE

--- mongodb/homework_3.py
+++ mongodb/homework_3.py
@@ -8,9 +8,32 @@ for doc in cursor:
 	db.movies.update({"_id" : doc["_id"]},{'$set':{"rated": "Pending rating"}});
 # Part B
 db.movies.insert_one({"title": "Vienna Waits For You","year": 2012, "countries": ["Austria", "Chile"],"genres": ["Short", "Comedy", "Horror", "Mystery"], "directors": ["Dominik Hartl"], "imdb": {"id":2171890, "rating":7.2, "votes":152}})
-for doc in db.movies.find({"title" : "Vienna Waits For You", "genres" : "Short"}):
-	print(doc)
+
 # Part C
-for doc in db.movies.aggregate([{'$match':{"genres" : "Short"}},{'$group':{"total":{'$sum': 1}, "_id" : "Short"}}])
+db.movies.aggregate([{'$match':{"genres" : "Short"}},{'$group':{"total":{'$sum': 1}, "_id" : "Short"}}])
 
 # Part D
+
+db.movies.aggregate([{'$match':{"countries" : "Hungary", "rated" : "Pending rating"}},{'$group':{"count":{'$sum': 1}, "_id" : {"countries" : "Hungary", "rating": "Pending rating"}}}])
+
+# Part E
+db.A.insert({"title": "A","year": 2012})
+db.A.insert({"title": "A","year": 2013})
+db.A.insert({"title": "A","year": 2014})
+db.A.insert({"title": "A","year": 2015})
+db.B.insert({"title": "B","year": 2015})
+db.B.insert({"title": "B","year": 2014})
+db.B.insert({"title": "A","year": 2013})
+db.B.insert({"title": "A","year": 2012})
+
+result = db.B.aggregate([{
+   '$lookup':
+     {
+       'from': "A",
+       "localField": "title",
+       "foreignField": "title",
+       'as': "output"
+     }
+}])
+
+print(list(result))
