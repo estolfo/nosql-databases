5f9bb96796bc3790902319b2d67d9ee465af9713
Emily <emily@mongodb.com>
2018-04-28 15:11:10 -0400

    Final project skeleton files

---
+++ cassandra/final_project.rb
@@ -0,0 +1,37 @@
+# Put the use case you chose here. Then justify your database choice:
+#
+#
+# Explain what will happen if coffee is spilled on one of the servers in your cluster, causing it to go down.
+#
+#
+# What data is it not ok to lose in your app? What can you do in your commands to mitigate the risk of lost data?
+#
+#
+
+
+
+# Action 1: <describe the action here>
+
+
+# Action 2: <describe the action here>
+
+
+# Action 3: <describe the action here>
+
+
+# Action 4: <describe the action here>
+
+
+# Action 5: <describe the action here>
+
+
+# Action 6: <describe the action here>
+
+
+# Action 7: <describe the action here>
+
+
+# Action 8: <describe the action here>
+
+
+
